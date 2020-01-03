103c48257ab3c3b3ba5b4ed1ed83b3d897d19998
ashwani65 <ashwanisinghlpu66@gmail.com>
2020-01-03 19:05:21 +0530

    Updates

---
+++ precache-manifest.5b2f7bf39afc784731e4b91c48009949.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d87bc3454d953656ef8836ea66bfde3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b192dea0018db308f8",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "923a30ed604e847e8b84",
+    "url": "/static/css/main.d66840af.chunk.css"
+  },
+  {
+    "revision": "95b192dea0018db308f8",
+    "url": "/static/js/2.f8356082.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f8356082.chunk.js.LICENSE"
+  },
+  {
+    "revision": "923a30ed604e847e8b84",
+    "url": "/static/js/main.bc7b3208.chunk.js"
+  },
+  {
+    "revision": "b48c8c345026200deff2",
+    "url": "/static/js/runtime-main.2b929124.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
